51d587970e15c952dd54fe5fd77dcdd0590aa103
RajkumarOjha <rajaojha9732@email.com>
2024-07-26 15:59:03 +0530

    original code

--- Phase-2/index.js
+++ Phase-2/index.js
@@ -1,10 +1,11 @@
-function iphone1(ASIN, color, display, camera){
+function iphone2(ASIN, color, display, camera, bluetooth){
     let obj  = {};
 
     obj.ASIN = ASIN;
     obj.color = color;
     obj.display = display;
     obj.camera = camera;
+    obj.bluetooth = bluetooth;
 
     obj.dial  = function(){
         console.log("tring..tring..")
@@ -17,9 +18,14 @@ function iphone1(ASIN, color, display, camera){
     obj.cameraClick = function(){
         console.log("Camera clicked")
     }
+
+    obj.bluetooth = function(){
+        console.log("Bluetooth connected successfully...")
+
+    }
     return obj;
 
 }
 
-let i1 = iphone1("B09X67JBQV", "Gray", 7.8, "2.0 MP")
-console.log(i1)
+let i2 = iphone2("B09X67JBQV", "Gray", 7.8, "2.0 MP","Bluetooth 5.1")
+console.log(i2.bluetooth());
